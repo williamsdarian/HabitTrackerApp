539182c40f5515124b2d9eb0696d49b16fc483e2
Darian Williams <daw023@uark.edu>
2024-12-04 11:32:56 -0600

    Inserted fake data for example habits, notification is now recurring each day, cancel/delete button functionality added

--- app/src/main/java/com/example/habittrackerapp/Repository/HabitRoomDatabase.kt
+++ app/src/main/java/com/example/habittrackerapp/Repository/HabitRoomDatabase.kt
@@ -55,10 +55,16 @@ public abstract class HabitRoomDatabase: RoomDatabase() {
             // Delete all content here.
             habitDao.deleteAll()
 
+            val dates = listOf<String>("2024-11-02", "2024-11-03", "2024-11-04", "2024-11-05", "2024-11-06", "2024-11-15", "2024-11-16", "2024-11-17", "2024-11-18", "2024-11-19", "2024-11-20", "2024-11-21",
+                "2024-11-22", "2024-11-23", "2024-11-24", "2024-11-25", "2024-11-26",
+                "2024-12-01", "2024-12-02")
+            val dates2 = listOf<String>("2024-11-01", "2024-11-02", "2024-11-03", "2024-11-04", "2024-11-09", "2024-11-10", "2024-11-11",
+                "2024-11-12", "2024-11-13", "2024-11-14", "2024-11-15", "2024-11-16", "2024-11-20", "2024-11-21", "2024-11-22",
+                "2024-11-28","2024-11-29","2024-11-30","2024-12-02", "2024-12-03")
             // Add sample words.
-            var word = Habit(null,"Hello", "Basic description", "", false, emptyList())
+            var word = Habit(null,"Explore new music", "Listen to a new album every day", "", false, dates)
             habitDao.insert(word)
-            word = Habit(null,"World!", "Lorem Ipsum Dolor...", "", false, emptyList())
+            word = Habit(null,"Meditate", "Once a day ", "", false, dates2)
             habitDao.insert(word)
 
 
